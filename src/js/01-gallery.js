6efa58618a2633418f561bdd2d6780fe9496925b
Roman <95884644+RomanTsiukh@users.noreply.github.com>
2022-06-05 11:15:31 +0300

    Task_1

---
+++ src/js/01-gallery.js
@@ -0,0 +1,27 @@
+// Add imports above this line
+import { galleryItems } from './gallery-items';
+
+import SimpleLightbox from "simplelightbox";
+import "simplelightbox/dist/simple-lightbox.min.css";
+// Change code below this line
+
+// 1. Add code in HTML
+const galery = document.querySelector(".gallery");
+const itemsMarkup = galleryItems.map( galleryItem => 
+    `<li class="gallery__item">
+        <a class="gallery__link" href="${galleryItem.original}">
+            <img class="gallery__image" src="${galleryItem.preview}" alt="${galleryItem.description}"/>
+        </a>
+    </li>`
+    ).join(""); 
+
+galery.insertAdjacentHTML('beforeend', itemsMarkup)
+
+
+// 2. Add library
+let gallery = new SimpleLightbox('.gallery a', 
+    {
+        captionsData: "alt", 
+        captionDelay: "250",
+    }
+);
